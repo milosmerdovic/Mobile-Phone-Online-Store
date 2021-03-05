150a6c11118630eef08d13c6a2fd502bdbf1f5fb
milos <milos.merdovic1@gmail.com>
2021-03-05 17:06:21 +0100

    Fixed Controller

--- src/main/java/com/test/demo/controllers/IndexController.java
+++ src/main/java/com/test/demo/controllers/IndexController.java
@@ -5,26 +5,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Controller {
+@Controller
+public class IndexController {
 
-    private static Logger logger = LoggerFactory.getLogger(Controller.class);
+    private static Logger logger = LoggerFactory.getLogger(IndexController.class);
     
     @Autowired
     private ProductRepository productRepository;
     
     @Autowired
-    public Controller (ProductRepository productRepository){
+    public IndexController (ProductRepository productRepository){
         this.productRepository = productRepository;
     }
     @RequestMapping("/")
     public String showAllProducts(Model model){
         model.addAttribute("products", productRepository.findAll());
         logger.info("Info log message:" + model.getAttribute("products").toString());
-        return "index";
+        return "/index";
     }
 }
