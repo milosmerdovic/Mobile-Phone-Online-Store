a720523a319749b9be86d35445ee4d36bfb8fb85
milos <milos.merdovic1@gmail.com>
2021-03-05 16:31:00 +0100

    third commit

--- src/main/java/com/test/demo/controllers/Controller.java
+++ src/main/java/com/test/demo/controllers/Controller.java
@@ -1,14 +1,18 @@
 package com.test.demo.controllers;
 
 import com.test.demo.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    private static Logger logger = LoggerFactory.getLogger(Controller.class);
     
     @Autowired
     private ProductRepository productRepository;
@@ -17,9 +21,10 @@ public class Controller {
     public Controller (ProductRepository productRepository){
         this.productRepository = productRepository;
     }
-
-    public String showAllProducs(Model model){
+    @RequestMapping("/")
+    public String showAllProducts(Model model){
         model.addAttribute("products", productRepository.findAll());
+        logger.info("Info log message:" + model.getAttribute("products").toString());
         return "index";
     }
 }
