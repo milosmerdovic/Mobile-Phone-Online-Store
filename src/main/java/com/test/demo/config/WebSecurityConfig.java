eff891f03a56fd419ac413e72e33a9e9f5abcc66
milos <milos.merdovic1@gmail.com>
2021-06-10 00:34:57 +0200

    Load css

--- src/main/java/com/test/demo/config/WebSecurityConfig.java
+++ src/main/java/com/test/demo/config/WebSecurityConfig.java
@@ -1,12 +1,9 @@
 package com.test.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +12,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure (HttpSecurity http) throws Exception{
         http
             .authorizeRequests()
-                    .antMatchers("/**").permitAll()
+                    .antMatchers("/", "/index", "/home", "/login", "/css/style.css", "/images/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
             .formLogin()
