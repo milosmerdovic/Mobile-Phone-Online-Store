41a0ba60b905eb284b5b7b7f9116e800b8bab27a
Milos <milos.merdovic1@gmail.com>
2021-06-30 16:06:08 +0200

    New changes

--- src/main/java/com/test/demo/entity/Order.java
+++ src/main/java/com/test/demo/entity/Order.java
@@ -1,11 +1,13 @@
 package com.test.demo.entity;
 
 import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
@@ -26,12 +28,6 @@ public class Order {
 	private String customerEmail;
 	private LocalDateTime createdAt = LocalDateTime.now();
 	
-	public Order(){}
-	
-	public Order(int id, LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-		this.id = id;
-	}
 	
 	public int getId() {
 		return id;
@@ -60,19 +56,19 @@ public class Order {
 	public void setCustomerEmail(String customerEmail) {
 		this.customerEmail = customerEmail;
 	}
-
+	
 	public String getCustomerAddress() {
 		return customerAddress;
 	}
 	public void setCustomerAddress(String customerAddress) {
 		this.customerAddress = customerAddress;
 	}
+	
 	public LocalDateTime getCreatedAt() {
 		return createdAt;
 	}
-
 	public void setCreatedAt(LocalDateTime createdAt) {
 		this.createdAt = createdAt;
 	}
-	
+
 }
